a2b4485873151829ecda6c6480260f816d7b20d8
karamihey <karina.mihey@gmail.com>
2016-07-07 23:29:33 +0200

    Update openday main page

--- openday/js/main.js
+++ openday/js/main.js
@@ -1,7 +1,32 @@
 jQuery(function() {
+	// Fixed top navigation
+	$(window).scroll(function() {
+		var top = $(document).scrollTop();
+
+		if ($(window).width() > 939) {
+			var fixedElem = $('.navigation-fixed');
+		}
+		else {
+			var fixedElem = $('.top-menu');
+		}
+		if (top < 80) {
+			fixedElem.css({top: '0', position: 'relative'});
+		}
+		else {
+			fixedElem.css({top: '0', position: 'fixed'});
+		}
+	});
+
 	// Select FormStyler
 	$('.js-select').styler({
 		selectSmartPositioning: false,
-		selectPlaceholder: 'Р›СЋР±РѕРµ'
+		selectPlaceholder: 'Все области'
+	});
+
+	// Slider
+	$('.js-slider').bxSlider({
+		controls: false,
+		maxSlides: 1,
+		slideWidth: 590
 	});
 });
