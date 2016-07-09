951e8ffef20ccc929100e92a38d83811e54e30c2
karamihey <karina.mihey@gmail.com>
2016-07-09 16:16:41 +0200

    Update openday main page

--- openday/js/main.js
+++ openday/js/main.js
@@ -29,4 +29,42 @@ jQuery(function() {
 		maxSlides: 1,
 		slideWidth: 590
 	});
+
+	var slider = $('.js-slider').bxSlider({
+		controls: false,
+		maxSlides: 1,
+		slideWidth: 590
+	});
+
+	onResize = function() {
+		slider.reloadSlider({
+			controls: false,
+			pager: false,
+			maxSlides: 1,
+			slideWidth: 590
+		});
+	}
+
+	$(window).resize(onResize);
+
+	// Mobile Menu
+	$('.j-mobile-menu-btn').sidr({
+		name: 'mobile-menu-content',
+		source: '.s-main, .s-catalog-goods, .s-catalog-services, .s-catalog, .s-top-menu'
+	});
+
+	$('.j-mobile-menu-btn').click(function() {
+		$('.mobile-menu-close').show();
+	});
+
+	$('.sidr-overlay, .mobile-menu-close').click(function() {
+		$.sidr('close', 'mobile-menu-content');
+		$('.mobile-menu-close').hide();
+	});
+
+	$('.sidr-class-catalog-menu-link, .sidr-class-catalog-menu-dropdown-submenu').click(function() {
+		$(this)
+			.toggleClass('submenu-opened')
+			.next().toggle();
+	});
 });
